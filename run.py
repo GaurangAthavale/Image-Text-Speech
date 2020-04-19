f01e77a2f3001195234f7f24befe9b3848ecb219
GaurangAthavale <gaurang.a@somaiya.edu>
2020-04-19 18:33:28 +0530

    i guess everything is done

--- run.py
+++ run.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request, send_from_directory
 from werkzeug.utils import secure_filename
 from tts import text2, voice2
 import os
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 app = Flask(__name__)
 yo = 'wfkhqhofqfcqfcq'
@@ -9,7 +12,7 @@ app.config['UPLOAD_FOLDER'] = './static/avatars/'
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('vision3.html')
 
 @app.route('/show', methods = ['GET','POST'])
 def text1():
@@ -25,10 +28,11 @@ def text1():
         if file:
             # imagename = secure_filename(image.imagename)
             print(app.config['UPLOAD_FOLDER']+str(filename[1]))
+            # path = app.config['UPLOAD_FOLDER']+str(filename[1])
             file.save(os.path.abspath(app.config['UPLOAD_FOLDER']+str(filename[1])))
         print(str(file))
         text = text2(file)
-        return render_template('index1.html', text_image = text)
+        return render_template('index1.html', text_image = text, path = filename[1] )
 
 @app.route('/voice',methods = ['GET','POST'])
 def voice():
@@ -36,8 +40,10 @@ def voice():
         # name = request.form['name']
         # print(name)
         file = request.files['img']
-        voice1 = voice2(file)
-        return render_template('voice.html', audios = voice1)
+        voice1,path = voice2(file)
+        path1 = './static/avatars/'+path
+        print(path1)
+        return render_template('voice.html', audios = voice1, path = path1)
 
 @app.route('/<path:path>')
 def send_js(path):
--- tts.py
+++ tts.py
@@ -2,9 +2,10 @@
 # will convert the image to text string 
 import pytesseract	 
 import os
+from werkzeug.utils import secure_filename
 # adds image processing capabilities 
 from PIL import Image	 
-
+import urllib3
 # converts the text to speech 
 # import pyttsx3		 
 
@@ -13,13 +14,17 @@ from googletrans import Translator
 
 from gtts import gTTS
 
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 # opening an image from the source path 
 def text2(image):
     print(image)
     img = Image.open(image)	
     pytesseract.pytesseract.tesseract_cmd = r'C:/Users/HP/AppData/Local/Tesseract-OCR/tesseract.exe'
     text = pytesseract.image_to_string(img)
-    print(text)
+    # print(text.split("\n"))
+    text = text.split("\n")
     return text
 
 def voice2(image):
@@ -31,11 +36,14 @@ def voice2(image):
     output = gTTS(text=text,lang=language, slow=False)
     filename = str(image).split("'")
     print(filename)
+    # z = filename[1].secure_filename(image.z)
+    path = './static/avatars/'+str(filename[1])
+    image.save(os.path.abspath(path))
     val = filename[1][:filename[1].index(".")]
     name = './static/audio/'+val+".mp3"
     if output:
         output.save(os.path.abspath(name))
     # return os.path.abspath(name)
-    return val+".mp3"
+    return val+".mp3",filename[1]
 
     # os.system("start "+name)
